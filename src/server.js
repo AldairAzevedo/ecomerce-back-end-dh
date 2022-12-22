0dfb1cb3ca17534ff1bfe0c02a64e029fceeab9f
AldairAzevedo <aldairazevedo@hotmail.com.br>
2022-12-22 16:33:24 -0400

    Produção da Aplicação

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ import sequelize from "./config/db.js";
 dotenv.config();
 
 try {
-  // sequelize.sync({force:true}); // comentar após criar as tabelas / {force:true} para força a recriação da modelagem
-  app.listen(process.env.APLICATION_PORT, () => {
+  sequelize.sync({force:true}); // comentar após criar as tabelas / {force:true} para força a recriação da modelagem
+  app.listen(process.env.PORT || process.env.APLICATION_PORT, () => {
     console.log(`App is running in port ${process.env.APLICATION_PORT}...`);
   });
 } catch (e) {
